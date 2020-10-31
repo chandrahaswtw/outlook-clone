329cd4e0d726c9cfff4afe7444397ed788bf1f66
chandrahaswtw <chandrahaswtw@gmail.com>
2020-10-31 16:02:21 +0530

    Updates

---
+++ precache-manifest.32bf6bb7e5e9bcaaa54dadfe43f97304.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889e6b10b487a2923cb73ea3178d12c2",
+    "url": "/outlook-clone/index.html"
+  },
+  {
+    "revision": "3eea89f7d9bc201b3a71",
+    "url": "/outlook-clone/static/css/main.29607a16.chunk.css"
+  },
+  {
+    "revision": "57de540ebd6100ab4e39",
+    "url": "/outlook-clone/static/js/2.217c3f35.chunk.js"
+  },
+  {
+    "revision": "0d6c4affe2a659999d44765f9a17cb4f",
+    "url": "/outlook-clone/static/js/2.217c3f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3eea89f7d9bc201b3a71",
+    "url": "/outlook-clone/static/js/main.73170d54.chunk.js"
+  },
+  {
+    "revision": "92236a23cc012a583cc6",
+    "url": "/outlook-clone/static/js/runtime-main.cc83303d.js"
+  },
+  {
+    "revision": "1025a6e0fb0fa86f17f57cc82a6b9756",
+    "url": "/outlook-clone/static/media/OpenSans-Bold.1025a6e0.ttf"
+  },
+  {
+    "revision": "2d0bdc8df10dee036ca3bedf6f3647c6",
+    "url": "/outlook-clone/static/media/OpenSans-Light.2d0bdc8d.ttf"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/outlook-clone/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/outlook-clone/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "0b6ed8720471b66c98f11bf3854fb4bb",
+    "url": "/outlook-clone/static/media/envelope.0b6ed872.svg"
+  },
+  {
+    "revision": "d9d021d5509f3e8f3f9ec0dfa1774a52",
+    "url": "/outlook-clone/static/media/nomail.d9d021d5.svg"
+  }
+]);
